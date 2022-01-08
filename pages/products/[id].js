39145b3d4ed1e8f61071e0ba9850a11b9abcadad
Humberto Escobedo <hescobedo@hotmail.com>
2022-01-08 14:15:14 -0600

    Add Style to Product Page

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,6 +1,7 @@
 import Title from '../../components/Title';
 import Head from 'next/head';
 import { getProduct, getProducts } from '../../lib/products';
+import Image from 'next/image';
 
 export async function getStaticPaths(id) {
   const products = await getProducts(id);
@@ -13,18 +14,20 @@ export async function getStaticPaths(id) {
 }
 
 export async function getStaticProps({ params: { id } }) {
-  try{
-  const product = await getProduct(id);
-  return { props: { product }, revalidate: parseInt(process.env.REVALIDATE_SECONDS) };
-  } catch(err){
-      if(err instanceof ApiError && err.status===404){
-        return { notfound: true };
-      }
-      throw err;
+  try {
+    const product = await getProduct(id);
+    return {
+      props: { product },
+      revalidate: parseInt(process.env.REVALIDATE_SECONDS),
+    };
+  } catch (err) {
+    if (err instanceof ApiError && err.status === 404) {
+      return { notfound: true };
+    }
+    throw err;
   }
-
 }
-export default function ProductPage({product}) {
+export default function ProductPage({ product }) {
   return (
     <>
       <Head>
@@ -34,9 +37,21 @@ export default function ProductPage({product}) {
       </Head>
       <main className='px-6 py-4'>
         <Title>{product.title}</Title>
-        <p>
-            {product.description}
-        </p>
+        <div className='flex flex-col lg:flex-row'>
+          {' '}
+          <div>
+            <Image
+              src={product.pictureUrl}
+              alt=''
+              width={640}
+              height={480}
+            ></Image>
+          </div>
+          <div className='flex-1 lg:ml-4'mt-2>
+            <p className="text-sm">{product.description}</p>
+            <p className="text-lg font-bold">Price: {product.price}</p>
+          </div>
+         </div>
       </main>
     </>
   );
