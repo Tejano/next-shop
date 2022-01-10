dca4f39e442f83f72de5f17bd6477c022484a3dc
Humberto Escobedo <hescobedo@hotmail.com>
2022-01-09 18:07:44 -0600

    Fixed Error in api

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,8 +1,16 @@
-import React from 'react';
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import { fetchJson } from '../lib/api.js';
 export default function NavBar() {
   //const user = {name:'Alice'}
-  const user = undefined;
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    (async () => {
+     const user = await fetchJson('/api/user');
+    })();
+  }, []);
+
   return (
     <nav className='px-2 py-1 text-sm'>
       <ul className='flex gap-2'>
